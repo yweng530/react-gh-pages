688c47067a40f0a4d06d4bfe273ada2bd5666f35
Yuhua Weng <yuhua2@illinois.edu>
2023-03-20 22:27:37 -0500

    intro

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Introduction from './pages/Introduction';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
          <Route path="/" exact Component={Home}/>
+         <Route path="/introduction" exact Component={Introduction}/>
         </Routes>
       </Router>
     </div>
---
+++ my-app/src/pages/Introduction.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Introduction.css';
+
+function Introduction() {
+  return (
+    <div className='about'>
+      <div className='aboutTop'></div>
+      <div className='aboutBottom'>
+        <h1>ABOUT ME</h1>
+        <p>I am currently a junior in CS at UIUC. I was raised and born in Ningbo, China.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Introduction
